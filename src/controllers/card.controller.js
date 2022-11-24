2de335a0e91723724b28e6759b50de67780ad9ca
Gabriel Piqueras <rocamora_gabriel@hotmail.com>
2022-11-24 01:05:21 +0100

    Cambios logrados con éxito

--- src/controllers/card.controller.js
+++ src/controllers/card.controller.js
@@ -3,8 +3,23 @@ import { getConnection } from '../database/database.js';
 
 const getCards = async (req, res) => {
     const connection = await getConnection();
-    const result = await connection.query('SELECT * FROM cards');
-        
+    const name = req.query.name;
+    let result;
+
+    if (name) {
+        result = await connection.query(`SELECT id, name, small_img, large_img, cardmarket_url
+                                               FROM cards
+                                               WHERE name LIKE '%${name}%'
+                                               ORDER BY name ASC
+                                               LIMIT 100`);
+    } else {
+
+        result = await connection.query(`SELECT id, name,small_img, large_img, cardmarket_url
+                                               FROM cards
+                                               ORDER BY name ASC
+                                               LIMIT 100`);
+    }
+  
     res.json(result);
 }
 
